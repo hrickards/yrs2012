4ce7682b4ef2db74a832bb823b2511f7e26c9d00
Harry Rickards <hrickards@gmail.com>
2012-08-06 21:27:02 +0100

    Fix geospatial index of raw_ratings

--- scrape/ratings.rb
+++ scrape/ratings.rb
@@ -12,8 +12,7 @@ XML_PATH = "FHRS875en-GB.xml"
 XML_URL = "http://ratings.food.gov.uk/OpenDataFiles/FHRS875en-GB.xml"
 Crack::XML.parse(open(XML_PATH))["FHRSEstablishment"]["EstablishmentCollection"]["EstablishmentDetail"].each do |place|
   if place["Geocode"]
-    place["Geocode"]["Longitude"] = place["Geocode"]["Longitude"].to_f
-    place["Geocode"]["Latitude"] = place["Geocode"]["Latitude"].to_f
+    place['Geocode'] = [place["Geocode"]["Longitude"].to_f, place["Geocode"]["Latitude"].to_f]
   end
   @collection.insert place
 end
