f87e47dd77613253c21dcc32d4f2f487b470b9a0
Harry Rickards <hrickards@gmail.com>
2012-08-09 12:12:08 +0100

    Tweaks to SMS response

--- twilio/mobile.rb
+++ twilio/mobile.rb
@@ -8,22 +8,23 @@ class FUDMobile < Sinatra::Base
   get '/sms' do
     content_type :xml
 
-    query = PlaceSearch.search_wrapper params[:body]
+    query = PlaceSearch.search_wrapper params[:Body]
     puts query.inspect
     humanised_query = "#{query[:logo]} " if query[:logo]
-    header = "FUD found you the following #{humanised_query}restaurants:\n"
+    header = "FUD found you these #{humanised_query}restaurants:\n"
 
     @connection = Mongo::Connection.new
     @db = @connection['fud']
     @collection = @db['places']
     results = @collection.find(query).limit(5)
 
-    results = results.map { |p| "#{p["business_name"]}, near #{p["address_line1"]}" }.join "\n"
+    results = results.map { |p| "#{p["business_name"]}, near #{p["address_line1"]}" }
 
     twiml = <<EOF
 <?xml version="1.0" encoding="UTF-8" ?>  
 <Response> 
-  <Sms>#{header + results}</Sms>
+  <Sms>#{header + results[0..1].join("\n")}</Sms>
+  <Sms>#{results[2..-1].join "\n"}</Sms>
 </Response>
 EOF
   end
