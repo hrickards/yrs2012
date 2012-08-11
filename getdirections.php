65bec89f5e5da74763d9749d8a6160d3a222bcdf
Archiboldian Cartheniscope <archiboldian@gmail.com>
2012-08-11 07:46:12 +0100

    EXTREME GUI CHANGES!

---
+++ getdirections.php
@@ -0,0 +1,75 @@
+<?php
+
+function do_dump(&$var, $var_name = NULL, $indent = NULL, $reference = NULL)
+{
+    $do_dump_indent = "<span style='color:#666666;'>|</span> &nbsp;&nbsp; ";
+    $reference = $reference.$var_name;
+    $keyvar = 'the_do_dump_recursion_protection_scheme'; $keyname = 'referenced_object_name';
+    
+    // So this is always visible and always left justified and readable
+    echo "<div style='text-align:left; background-color:white; font: 100% monospace; color:black;'>";
+
+    if (is_array($var) && isset($var[$keyvar]))
+    {
+        $real_var = &$var[$keyvar];
+        $real_name = &$var[$keyname];
+        $type = ucfirst(gettype($real_var));
+        echo "$indent$var_name <span style='color:#666666'>$type</span> = <span style='color:#e87800;'>&amp;$real_name</span><br>";
+    }
+    else
+    {
+        $var = array($keyvar => $var, $keyname => $reference);
+        $avar = &$var[$keyvar];
+
+        $type = ucfirst(gettype($avar));
+        if($type == "String") $type_color = "<span style='color:green'>";
+        elseif($type == "Integer") $type_color = "<span style='color:red'>";
+        elseif($type == "Double"){ $type_color = "<span style='color:#0099c5'>"; $type = "Float"; }
+        elseif($type == "Boolean") $type_color = "<span style='color:#92008d'>";
+        elseif($type == "NULL") $type_color = "<span style='color:black'>";
+
+        if(is_array($avar))
+        {
+            $count = count($avar);
+            echo "$indent" . ($var_name ? "$var_name => ":"") . "<span style='color:#666666'>$type ($count)</span><br>$indent(<br>";
+            $keys = array_keys($avar);
+            foreach($keys as $name)
+            {
+                $value = &$avar[$name];
+                do_dump($value, "['$name']", $indent.$do_dump_indent, $reference);
+            }
+            echo "$indent)<br>";
+        }
+        elseif(is_object($avar))
+        {
+            echo "$indent$var_name <span style='color:#666666'>$type</span><br>$indent(<br>";
+            foreach($avar as $name=>$value) do_dump($value, "$name", $indent.$do_dump_indent, $reference);
+            echo "$indent)<br>";
+        }
+        elseif(is_int($avar)) echo "$indent$var_name = <span style='color:#666666'>$type(".strlen($avar).")</span> $type_color".htmlentities($avar)."</span><br>";
+        elseif(is_string($avar)) echo "$indent$var_name = <span style='color:#666666'>$type(".strlen($avar).")</span> $type_color\"".htmlentities($avar)."\"</span><br>";
+        elseif(is_float($avar)) echo "$indent$var_name = <span style='color:#666666'>$type(".strlen($avar).")</span> $type_color".htmlentities($avar)."</span><br>";
+        elseif(is_bool($avar)) echo "$indent$var_name = <span style='color:#666666'>$type(".strlen($avar).")</span> $type_color".($avar == 1 ? "TRUE":"FALSE")."</span><br>";
+        elseif(is_null($avar)) echo "$indent$var_name = <span style='color:#666666'>$type(".strlen($avar).")</span> {$type_color}NULL</span><br>";
+        else echo "$indent$var_name = <span style='color:#666666'>$type(".strlen($avar).")</span> ".htmlentities($avar)."<br>";
+
+        $var = $var[$keyvar];
+    }
+    
+    echo "</div>";
+}
+
+$url = 'http://maps.googleapis.com/maps/api/directions/json?origin='.$_GET['olong'].','.$_GET['olat'].'&destination='.$_GET['nlong'].','.$_GET['nlat'].'&sensor=false';
+$direct = file_get_contents($url);
+$direct = json_decode($direct);
+$further = $direct->routes[0]->legs[0]->steps;
+echo '<div class="directions_box">';
+foreach($further as &$direction){
+	echo $direction->html_instructions;
+	echo '<br/>';
+}
+echo '</div>';
+echo '|';
+echo $direct->routes[0]->legs[0]->steps[1]->polyline->points;
+
+?>
--- index.php
+++ index.php
@@ -15,8 +15,16 @@
 <script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
 <script type="text/javascript" src="js/jquery.mCustomScrollbar.js"></script>
  <script type="text/javascript" src="http://www.google.com/jsapi?key=AIzaSyA4_MbXZb7jP5e9luRnPZRzZuvJOMyRuVM"></script>
-
+<?php
+if(isset($_GET['PHONEMODE'])){
+	<link rel="stylesheet" type="text/css" href="phone.css" />
+}
+?>
 <script>
+
+var selected = -1;
+
+var funcarr = [];
 
 function updateResizeDiv(){
 	var mapwidther = $(window).width()-$('#comparebox').width();
@@ -29,7 +37,9 @@ function updateResizeDiv(){
 	$('#map').css('left',$('#comparebox').width());
 	$('#map').css('height',mapheighter);
 	$('#introbox_inner').css('left',((mapwidther/2)-($('#introbox_inner').width()/2)));
-	$('#introbox_inner').css('top',((mapheighter/2)-($('#introbox_inner').height()/2)));
+	$('#introbox_inner').css('top',((mapheighter/2)-($('#introbox_inner').height()/2))-$('#searchbox').height());
+	$('#introbox_inner_manual').css('left',((mapwidther/2)-($('#introbox_inner_manual').width()/2)));
+	$('#introbox_inner_manual').css('top',((mapheighter/2)-($('#introbox_inner_manual').height()/2))-$('#searchbox').height());
 	$('#comparebox').css('height',$(window).height()-$('#searchbox').height()-25);
 }
 
@@ -41,11 +51,18 @@ function init_jsfud(){
 	?>
 	updateResizeDiv();
 	$("#comparebox").mCustomScrollbar();
-	$(".infobox").each(
-	function (i,elem) {
-		elem.mCustomScrollbar();
-	});
 	$('#introbox_inner').delay(800).fadeIn();
+	$("#manual_searchdiv").click(function() {
+		$("#introbox_inner_manual").fadeIn();
+	});
+
+	$("#intro_search_closebutton").click(function() {
+		$('#introbox_inner').fadeOut();
+	});
+
+	$("#manual_search_closebutton").click(function() {
+		$("#introbox_inner_manual").fadeOut();
+	});
 }
 
 $(window).resize(updateResizeDiv);
@@ -85,12 +102,26 @@ for($i=1;$i < (($cursorlimit+1) > $cursor->count() ? $cursor->count() : ($cursor
 	$cur = $cursor->getNext();
 	$place_items .= '[\'<div class="infobox" >';
 	
-	$comp_items .= '<div class="compbox" >';
+	$comp_items .= '<div class="compbox" id="compbox_'.$i.'" >';
+	
+	add_script('
+	
+	$("#compbox_'.$i.'").click(function(){
+		if(selected!=-1){
+			$("#compbox_"+selected).attr("class", "compbox");
+		}
+		infowindow.setContent(stripslashes(locations['.($i-1).'][0]));
+        infowindow.open(map, marker_array['.($i-1).']);
+        $("#compbox_'.$i.'").attr("class", "compbox selected");
+        selected = '.$i.';
+	});
+	
+	');
 	
 	add_field(true,'name', '<strong>'.addslashes($cur['name']).'</strong> ');
 	add_field(true,'rating', get_stars('<strong>Official Hygiene Rating: </strong>',intval($cur['rating_value']),'img/star_enabled.png','img/star_disabled.png'));
-	add_field(true,'googleplacesrating', get_stars('<strong>Google Places Rating: </strong> ('.$cur['rating'].') ', floor(intval($cur['rating_value'])),'img/star_enabled.png','img/star_disabled.png'));
-	add_field(true,'type','<strong>Type: </strong><br/>'.$cur['business_type']);
+	if($cur['rating']){add_field(true,'googleplacesrating', get_stars('<strong>Google Places Rating: </strong> ('.$cur['rating'].') ', floor(intval($cur['rating'])),'img/star_enabled.png','img/star_disabled.png'));};
+	add_field(true,'type','<strong>Type: </strong>'.$cur['business_type']);
 	add_field(false,'website','<strong>Website: </strong><br/><a target="_blank" href="'.$cur['website'].'" >'.$cur['website'].'</a>');
 	add_field(false,'number', '<strong>Phone Number: </strong><br/>'.$cur['formatted_phone_number']);
 	add_field(false,'intnumber', '<strong>Int. Phone Number: </strong><br/>'.$cur['international_phone_number']);
@@ -98,6 +129,18 @@ for($i=1;$i < (($cursorlimit+1) > $cursor->count() ? $cursor->count() : ($cursor
 		add_field(true,'allergyinfo', '<strong>Allergy Information: </strong><br/>'.get_allergies($cur['allergies'],'img/warning_enabled.png','img/warning_disabled.png'));
 	}
 	add_field(false,'address','<strong>Address: </strong><br/>'.addslashes(str_replace(', ',',<br/>',$cur['formatted_address'])));
+	if($frompos){
+		add_field(false,'directions', '<strong>Directions: </strong><br/><div class="directionsdiv" id="directionsdiv_'.$i.'" >Loading directions...</div>');
+		add_script('function loadDirectionsFor'.$i.'(){
+			$.get("getdirections.php?olong='.$frompos['1'].'&olat='.$frompos['0'].'&nlong='.$cur['location']['longitude'].'&nlat='.$cur['location']['latitude'].'", function(data) {
+				
+				data = data.split("|");
+				$("#directionsdiv_'.($i+1).'").html(data[0]);
+			
+			});
+		}
+		funcarr['.$i.'] = loadDirectionsFor'.$i.';');
+	}
 	
 	$comp_items .= '</div>';
 	
@@ -123,10 +166,12 @@ $comp_items .= '<div id = "compbox_spacer" style = "height:5px;width:100%;"></di
 		
 		<div style="width:100%; text-align:center;">
 		<img src="img/fud_logo_large.png" /><br/>
-			<font id = "ask_box" >what do you want to eat?</font><br/><br/>
+			<font id = "ask_box" >where do you want to eat?</font><br/><br/>
 				<div><form id="searchform_intro" method="post" action="http://infinite-island-5869.herokuapp.com/search" ><input id="searchbox_intro" type="text" name="query" placeholder="Enter a search query and press enter..." ></input><input type="submit" style="height:0px;width:0px;visibility:hidden;"></input></form><div id="manual_searchdiv" style="position:absolute;top:163px;left:58px" >+ manual search</div></div>
 			<font id = "explanation_box" >e.g. "Indian Takeaway Near Me" or<br/>"Indian Takeaway Near Brighton Marina"<br/>("near me" tells Fud to automatically detect your location)</font>
 			</div>
+	</div>
+	<div id = "introbox_inner_manual" style="display:none;position:absolute;" >
 	</div>
 </div>
 	
@@ -149,6 +194,7 @@ $comp_items .= '<div id = "compbox_spacer" style = "height:5px;width:100%;"></di
 <div id="map" style="position:absolute;left:330px;width:400px;height:100%;z-index:10;" ></div>
 
   <script type="text/javascript">
+  	
     var locations = [
 		<?php
 			echo $place_items;
@@ -200,48 +246,79 @@ $comp_items .= '<div id = "compbox_spacer" style = "height:5px;width:100%;"></di
 	
     
     map.setOptions({styles: styles});
+    
+    
+    var poly;
+  	
+  	var polyOptions = {
+    	strokeColor: '#000000',
+		strokeOpacity: 1.0,
+    	strokeWeight: 3
+	}
+	poly = new google.maps.Polyline(polyOptions);
+	poly.setMap(map);
+	var path = poly.getPath();
+    
+    
+    var marker_array = [];
 
     var infowindow = new google.maps.InfoWindow();
 
-    var marker, i, ibase;
+    var i, ibase;
     ibase=0;
     <?php
     if($frompos){
     ?>
     
     ibase = 1;
-    marker = new google.maps.Marker({
+    marker_array[0] = new google.maps.Marker({
         position: new google.maps.LatLng(<?php echo $frompos['1'].','.$frompos['0']; ?>),
         map: map,
         icon: markericon_me
     });
 
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    google.maps.event.addListener(marker_array[0], 'click', (function(marker, i) {
         return function() {
           infowindow.setContent('That\'s <a href = "http://www.youtube.com/watch?v=aEQcsuXnnnc" target="_blank" >you</a> that is,</br>that\'s your favourite <a href = "http://www.youtube.com/watch?v=IzdI_PecYx0" target="_blank" >food</a>.');
           infowindow.open(map, marker);
         }
-    })(marker, i));
+    })(marker_array[0], i));
     
     <?php
     }
     ?>
 
     for (i = ibase; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
+      marker_array[i] = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map,
         icon: markericon_pizza
       });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      google.maps.event.addListener(marker_array[i], 'click', (function(marker, i) {
         return function() {
+          if(selected!=-1){
+			$("#compbox_"+selected).attr("class", "compbox");
+		  }
           infowindow.setContent(stripslashes(locations[i][0]));
           infowindow.open(map, marker);
+          $("#compbox_"+i).attr("class", "compbox selected");
+          $('#comparebox').animate({scrollTop:$("#compbox_"+i).position().top}, 'slow');
+          selected = i;
+          <?php if($frompos){ ?>
+          	funcarr[i]();
+          <?php } ?>
         }
-      })(marker, i));
+      })(marker_array[i], i));
     }
   </script>
-	
+
+<script>
+$(document).ready(function(){
+	<?php
+		echo $script_items;
+	?>
+});
+</script>
 </body>
 </html>
